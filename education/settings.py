860e685cb4b037bf8b3c1a20b63023f6ed0f6ebe
BraFei <1836088871@qq.com>
2017-10-18 19:39:48 +0800

    django-by-example第十章第一篇

--- education/settings.py
+++ education/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'education.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'course/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
